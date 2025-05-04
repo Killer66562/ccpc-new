9f29acecf4b65a5e70625e65271e0fd8c4b59f11
Killer66562 <jimmy688382@gmail.com>
2025-05-04 23:19:05 +0800

    Add bootstrap bundle js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import AppLayout from './layouts/AppLayout.vue'
 import Vue3Toastify from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
