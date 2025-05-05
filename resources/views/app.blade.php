911d4400099efc3990b5485a2e0c1cea5d62ef45
Killer66562 <jimmy688382@gmail.com>
2025-05-05 14:25:04 +0800

    Add title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>中區大專院校程式設計競賽</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         @routes
